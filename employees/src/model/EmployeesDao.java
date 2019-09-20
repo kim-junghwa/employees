da25486074a65a42090a2fd5952fed650076bf00
GD3 <GD3@DESKTOP-NF6773I>
2019-09-20 16:27:19 +0900

    EmployeesDao.java 제작

--- employees/src/model/EmployeesDao.java
+++ employees/src/model/EmployeesDao.java
@@ -1,7 +1,54 @@
 package model;
 import java.sql.*;
+import java.util.*;
+import vo.*;
 
 public class EmployeesDao {
+
+	public List<Employees> selectEmployeesList(int limit) {
+		List<Employees> list = new ArrayList<Employees>();
+		Connection conn = null;
+		PreparedStatement stmt = null;
+		ResultSet rs = null;
+		String sql = "select emp_no, birth_date, first_name, last_name, gender, hire_date from employees limit ?";
+		try {
+			Class.forName("org.mariadb.jdbc.Driver");
+			conn = DriverManager.getConnection("jdbc:mariadb://localhost:3306/employees", "root", "java1234");
+			stmt = conn.prepareStatement(sql);
+			stmt.setInt(1, limit);
+			rs = stmt.executeQuery();
+			
+			while (rs.next()) {
+				Employees employees = new Employees();
+				employees.setEmpNo(rs.getInt("emp_no"));
+				employees.setBirthDate(rs.getString("birth_date"));
+				employees.setFirstName(rs.getString("first_name"));
+				employees.setLastName(rs.getString("last_name"));
+				employees.setGender(rs.getString("gender"));
+				employees.setHireDate(rs.getString("hire_date"));
+				list.add(employees);
+			}
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+		finally {
+			try {
+				rs.close();
+				stmt.close();
+				conn.close();
+			}
+			catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		
+		return list;
+	}
+	
+	
+	
+	
 	public int selectEmployeesCount() {
 		int count = 0;
 		final String sql = "select count(*) from employees";
