eca7fe24f0329615f66bf8536ec1cda5a6cc36c3
GD3 <GD3@DESKTOP-NF6773I>
2019-09-23 16:58:12 +0900

    테이블의 DAO 객체 생성후 selectRowCount 메소드 호출 request에 값 저장

--- employees/src/controller/IndexServlet.java
+++ employees/src/controller/IndexServlet.java
@@ -9,10 +9,38 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/index")
+@WebServlet({"/index","/"})
 public class IndexServlet extends HttpServlet {	
+	private EmployeesDao employeesDao;
+	private DepartmentsDao departmentsDao;
+	private DeptEmpDao deptEmpDao;
+	private DeptManagerDao deptManagerDao;
+	private SalariesDao salariesDao;
+	private TitlesDao titlesDao;
+	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		//System.out.println("/index RUL 요청");
+		
+		employeesDao = new EmployeesDao();
+		departmentsDao = new DepartmentsDao();
+		deptEmpDao = new DeptEmpDao();
+		deptManagerDao = new DeptManagerDao();
+		salariesDao = new SalariesDao();
+		titlesDao = new TitlesDao();
+		
+		int employeesRowCount = employeesDao.selectEmployeesRowCount();
+		int departmentsRowCount = departmentsDao.selectDepartmentsRowCount();
+		int deptEmpRowCount = deptEmpDao.selectDeptEmpRowCount();
+		int deptManagerRowCount = deptManagerDao.selectDeptManagerRowCount();
+		int salariesRowCount = salariesDao.selectSalariesRowCount();
+		int titlesRowCount = titlesDao.selectTitlesRowCount();
+		
+		request.setAttribute("employeesRowCoutn", employeesRowCount);
+		request.setAttribute("departmentsRowCount", departmentsRowCount);
+		request.setAttribute("deptEmpRowCount", deptEmpRowCount);
+		request.setAttribute("deptManagerRowCount", deptManagerRowCount);
+		request.setAttribute("salariesRowCount", salariesRowCount);
+		request.setAttribute("titlesRowCount", titlesRowCount);
 		
 		// /WEB-INF/views/index.jsp
 		// request.getRequestDispatcher("/WEB-INF/views/index.jsp").forward(request, response); <- 가능
