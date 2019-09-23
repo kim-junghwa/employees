9599dc364f6a3e35f3b0aed1b310a00634f789c0
GD3 <GD3@DESKTOP-NF6773I>
2019-09-23 16:55:29 +0900

    selectRowCoutn 메소스 생성

--- employees/src/model/EmployeesDao.java
+++ employees/src/model/EmployeesDao.java
@@ -46,10 +46,57 @@ public class EmployeesDao {
 		return list;
 	}
 	
+	public List<Employees> selectEmployeesListOrderBy(String order) {
+		List<Employees> list = new ArrayList<Employees>();
+		
+		Connection conn = null;
+		PreparedStatement stmt = null;
+		ResultSet rs = null;
+		String sql = null;
+		if(order.equals("asc")) {
+			sql = "select emp_no, birth_date, first_name, last_name, gender, hire_date from employees order by first_name asc limit 50";
+		}
+		else if (order.equals("desc")) {
+			sql = "select emp_no, birth_date, first_name, last_name, gender, hire_date from employees order by first_name desc limit 50";
+		}
+		
+		try {
+			Class.forName("org.mariadb.jdbc.Driver");
+			conn = DriverManager.getConnection("jdbc:mariadb://localhost:3306/employees", "root", "java1234");
+			stmt = conn.prepareStatement(sql);
+			rs = stmt.executeQuery();
+			
+			while (rs.next()) {
+				Employees employees = new Employees();
+				employees.setEmpNo(rs.getInt("emp_no"));
+				employees.setBirthDate(rs.getString("birth_date"));
+				employees.setFirstName(rs.getString("first_name"));
+				employees.setLastName(rs.getString("last_name"));
+				employees.setGender(rs.getString("gender"));
+				employees.setHireDate(rs.getString("hire_date"));
+				list.add(employees);
+			}
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+		finally {
+			try {
+				rs.close();
+				stmt.close();
+				conn.close();
+			}
+			catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		
+		
+		return list;
+	}
 	
 	
-	
-	public int selectEmployeesCount() {
+	public int selectEmployeesRowCount() {
 		int count = 0;
 		final String sql = "select count(*) from employees";
 		Connection conn = null;
