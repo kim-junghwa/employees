663ee715a0cd77849bff474c49ae78aee671dce5
GD3 <GD3@DESKTOP-NF6773I>
2019-09-23 16:55:12 +0900

    각각 테이블의 DAO 생성후 selectRowCount메소스 생성

---
+++ employees/src/model/TitlesDao.java
@@ -0,0 +1,39 @@
+package model;
+
+import java.sql.*;
+
+public class TitlesDao {
+	public int selectTitlesRowCount() {
+		int count = 0;
+		Connection conn = null;
+		PreparedStatement stmt = null;
+		ResultSet rs = null;
+		String sql = "select count(*) cnt from titles";
+		
+		try {
+			Class.forName("org.mariadb.jdbc.Driver");
+			conn = DriverManager.getConnection("jdbc:mariadb://localhost:3306/employees", "root", "java1234");
+			stmt = conn.prepareStatement(sql);
+			rs = stmt.executeQuery();
+			
+			if(rs.next()) {
+				count = rs.getInt("cnt");
+			}
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+		finally {
+			try {
+				rs.close();
+				stmt.close();
+				conn.close();
+			}
+			catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		
+		return count;
+	}
+}
