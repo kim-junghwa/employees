7fc78631f9157638f94106c95d6c2a41d32239df
GD3 <GD3@DESKTOP-NF6773I>
2019-09-23 16:59:38 +0900

    EmployeesDao 객체 생성후 list에 selectEmployeesListOrderBy 메소드 리턴값 저장 employeesListOrderBy.jsp 호출

---
+++ employees/src/controller/GetEmployeesListOrderByServlet.java
@@ -0,0 +1,27 @@
+package controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import vo.*;
+import java.util.*;
+import model.*;
+
+@WebServlet("/employees/employeesListOrderBy")
+public class GetEmployeesListOrderByServlet extends HttpServlet {
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		EmployeesDao employeesDao = new EmployeesDao();
+		String order = request.getParameter("order");
+		//System.out.println(order);
+		
+		List<Employees> list = employeesDao.selectEmployeesListOrderBy(order);
+		
+		request.setAttribute("list", list);
+		
+		request.getRequestDispatcher("/WEB-INF/views/employees/employeesListOrderBy.jsp").forward(request, response);
+	}
+}
