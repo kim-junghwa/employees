9ca59be6cd86d7b66a15bd625e35389447bb6a83
GD3 <GD3@DESKTOP-NF6773I>
2019-09-24 13:28:28 +0900

    데이터베이스에서 Salaries테이블의 통계값을 출력할 메소드와 servlet jsp를 제작

--- employees/src/model/SalariesDao.java
+++ employees/src/model/SalariesDao.java
@@ -1,6 +1,7 @@
 package model;
 
 import java.sql.*;
+import java.util.*;
 
 public class SalariesDao {
 	public int selectSalariesRowCount() {
@@ -49,4 +50,58 @@ public class SalariesDao {
 		
 		return count;
 	}
+	
+	public Map<String, Long> selectSalariesStatistics() {
+		Map<String, Long> map = new HashMap<String, Long>();
+		
+		//데이터베이스 연결을 위한변수 선언
+		Connection conn = null;
+		PreparedStatement stmt = null;
+		ResultSet rs = null;
+		
+		//쿼리문 생성
+		String sql = "select count(salary),sum(salary),avg(salary),max(salary),min(salary),std(salary) from salaries";
+		
+		//try catch문으로 예외 처리
+		try {
+			
+			//데이터베이스 연결
+			Class.forName("org.mariadb.jdbc.Driver");
+			conn = DriverManager.getConnection("jdbc:mariadb://localhost:3306/employees", "root", "java1234");
+			
+			//쿼리문 저장, 실행후 결과값 저장
+			stmt = conn.prepareStatement(sql);
+			rs = stmt.executeQuery();
+			
+			while (rs.next()) {
+				//map에 함수값 저장
+				map.put("count", rs.getLong("count(salary)"));
+				map.put("sum", rs.getLong("sum(salary)"));
+				map.put("avg", rs.getLong("avg(salary)"));
+				map.put("max", rs.getLong("max(salary)"));
+				map.put("min", rs.getLong("min(salary)"));
+				map.put("std", rs.getLong("std(salary)"));
+			}
+		}
+		catch (Exception e) {
+			//예외 발생시 콜솔창에 출력
+			e.printStackTrace();
+		}
+		finally {
+			try {
+				//무조건 실행
+				rs.close();
+				stmt.close();
+				conn.close();
+			}
+			catch (Exception e) {
+				//예외 발생시 콜솔창에 출력
+				e.printStackTrace();
+			}
+		}
+	
+		return map;
+	}
+
+
 }
