db66449633abda1e8ff1d99f6356c356cbec14bb
GD3 <GD3@DESKTOP-NF6773I>
2019-09-24 13:27:31 +0900

    데이터베이스에서 titles테이블에 중복값을 제거하느 메소드와 servlet을 제작

--- employees/src/model/TitlesDao.java
+++ employees/src/model/TitlesDao.java
@@ -1,6 +1,8 @@
 package model;
 
+import java.util.*;
 import java.sql.*;
+import db.*;
 
 public class TitlesDao {
 	public int selectTitlesRowCount() {
@@ -18,8 +20,7 @@ public class TitlesDao {
 		try {
 			
 			//데이터베이스 연결
-			Class.forName("org.mariadb.jdbc.Driver");
-			conn = DriverManager.getConnection("jdbc:mariadb://localhost:3306/employees", "root", "java1234");
+			conn = DBHelper.getConnection();
 			
 			//쿼리문 저장, 실행후 결과값 저장
 			stmt = conn.prepareStatement(sql);
@@ -35,18 +36,47 @@ public class TitlesDao {
 			e.printStackTrace();
 		}
 		finally {
-			try {
-				//무조건 실행
-				rs.close();
-				stmt.close();
-				conn.close();
-			}
-			catch (Exception e) {
-				//예외 발생시 콜솔창에 출력
-				e.printStackTrace();
-			}
+			DBHelper.close(rs, stmt, conn);
+		
 		}
 		
 		return count;
 	}
+	
+	public List<String> selectTitlesListDistinct() {
+		List<String> list = new ArrayList<String>();
+
+		//데이터베이스 연결을 위한변수 선언
+		Connection conn = null;
+		PreparedStatement stmt = null;
+		ResultSet rs = null;
+		
+		//쿼리문 생성 변경 불가
+		final String sql = "select distinct title from titles";
+		
+		//try catch문으로 예외 처리
+		try {
+			
+			//데이터베이스 연결
+			conn = DBHelper.getConnection();
+			
+			//쿼리문 저장, 실행후 결과값 저장
+			stmt = conn.prepareStatement(sql);
+			rs = stmt.executeQuery();
+			
+			while (rs.next()) {
+				list.add(rs.getString("title"));
+			}
+		}
+		catch (Exception e) {
+			//예외 발생시 콜솔창에 출력
+			e.printStackTrace();
+		}
+		finally {
+			DBHelper.close(rs, stmt, conn);
+		
+		}
+		return list;
+	}
+	
 }
